20810bc2535ab803bbb83b92c4f7eb438bccdcfe
PentaMoses <matheusrocha182@gmail.com>
2025-05-31 03:14:54 -0300

    Update App.tsx
    Signed-off-by: PentaMoses <matheusrocha182@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import ViralPromoKit from "./pages/ViralPromoKit"; // <-- Add this import if not present
+import ViralPromoKit from "./pages/ViralPromoKit"; // Make sure this component exists
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,7 +16,7 @@ const App = () => (
       <Sonner />
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/viral-promo-kit" element={<ViralPromoKit />} /> {/* <-- Add this route */}
+        <Route path="/viral-promo-kit" element={<ViralPromoKit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </TooltipProvider>
